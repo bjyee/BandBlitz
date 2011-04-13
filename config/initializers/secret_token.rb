952c941de3f297f98f1b6f1b8a877eb47ff91d5c
Brandon Yee <bjyee@andrew.cmu.edu>
2011-04-13 19:20:07 -0400

    basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BandBlitz::Application.config.secret_token = 'a356550a5250f509bf467d1c2a496ca387301d47076b0835b1c142d7ae6f574baa6b401a9487e3c7e9e066c7df1713a0e25abece656eedaad0921de0ddd640da'
